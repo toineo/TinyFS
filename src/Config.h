bf50478fe7f1c9f22b5fa433ee1607048cbb09f2
t <toineo@users.noreply.github.com>
2015-06-02 17:39:06 -0400

    Set up

---
+++ src/Config.h
@@ -0,0 +1,17 @@
+/*
+ * Config.h
+ *
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+// Size of the disk blocks (for now, exactly disk sectors)
+#define DBSize 1024
+
+// TODO: implement a frame size, not necessarily equal to DBSize
+
+// TODO: link this
+#define MemPageSize 4096
+
+#endif /* CONFIG_H_ */
